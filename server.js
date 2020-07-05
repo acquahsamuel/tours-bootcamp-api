4bfa9fc2b63642a784d1988957e8b697ba7543d4
acquahsamuel <micropplejame01@gmail.com>
2020-07-05 07:46:32 +0000

    update on server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const dotenv = require('dotenv');
-const app = require('./app');
+const dotenv = require('dotenv')
+const app = require('./app')
 
-dotenv.config({path :`./config.env`});
-console.log(app.get('env')); 
-console.log(process.env);
+dotenv.config({ path: './config.env' })
 
-const port = 8000 || process.env.PORT;
-app.listen(`${port}`, () => {
-    console.log(`Listening on port ${port}`)
-});
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`)
+})
