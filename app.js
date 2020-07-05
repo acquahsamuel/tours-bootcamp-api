5efc26d58b4eed3b7b33d2b8b4d8bed56fc5251d
acquahsamuel <micropplejame01@gmail.com>
2020-07-05 07:46:11 +0000

    update on app.js

--- app.js
+++ app.js
@@ -1,40 +1,30 @@
 const express = require('express')
 const morgan = require('morgan')
 
-const app = express();
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
-/* Middlewares */
+const app = express()
+
+// 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
 
-
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
-// Development or Production Environments
-
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-
+app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  next()
 })
 
-app.use('/api/V1/tours', tourRouter);
-app.use('/api/V1/users', userRouter);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString()
+  next()
+})
 
-module.exports = app;
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
-
-/*
- Representational State Transfer 
- Enveloping
- API should be stateless  ** 
- How stateless API works 
-
- Routes handler
- Response Cycle 
-*/
+module.exports = app
