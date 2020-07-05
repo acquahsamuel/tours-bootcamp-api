b6547ddfc9e72a1fbc3ceb0ab227e265a0620b48
acquahsamuel <micropplejame01@gmail.com>
2020-07-05 07:45:31 +0000

    controller update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,35 +1,33 @@
-const fs = require('fs');
+const fs = require('fs')
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+)
 
 exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
+  console.log(`Tour id is: ${val}`)
 
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID'
-    });
+    })
   }
-  next();
-};
+  next()
+}
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
       message: 'Missing name or price'
-    });
+    })
   }
-  next();
-};
-
-
+  next()
+}
 
 exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
+  console.log(req.requestTime)
 
   res.status(200).json({
     status: 'success',
@@ -38,33 +36,33 @@ exports.getAllTours = (req, res) => {
     data: {
       tours
     }
-  });
-};
+  })
+}
 
 exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
+  console.log(req.params)
+  const id = req.params.id * 1
 
-  const tour = tours.find(el => el.id === id);
+  const tour = tours.find(el => el.id === id)
 
   res.status(200).json({
     status: 'success',
     data: {
       tour
     }
-  });
-};
+  })
+}
 
 exports.createTour = (req, res) => {
   // console.log(req.body);
 
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
 
-  tours.push(newTour);
+  tours.push(newTour)
 
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     err => {
       res.status(201).json({
@@ -72,10 +70,11 @@ exports.createTour = (req, res) => {
         data: {
           tour: newTour
         }
-      });
+      })
+      console.log(err)
     }
-  );
-};
+  )
+}
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
@@ -83,12 +82,12 @@ exports.updateTour = (req, res) => {
     data: {
       tour: '<Updated tour here...>'
     }
-  });
-};
+  })
+}
 
 exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
     data: null
-  });
-};
+  })
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,44 +1,55 @@
 const fs = require('fs')
 
-const users = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/users.json`))
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/users.json`)
+)
 
 exports.getAllUsers = (req, res) => {
-  console.log(req.requestTime);
-
-  res.status(200).json({
+  res.status(500).json({
     status: 'success',
-    requestedAt: req.requestTime,
+    requestedAt: req.requestedTime,
     results: users.length,
     data: {
       users
     }
-  });
-};
-
-
+  })
+}
 exports.getUser = (req, res) => {
-    console.log(req.params);
+  const id = req.params.id * 1
 
-    // Trick to convert string to number
-    const id = req.params.id * 1;
-    const user = users.find(el => el.id === id);
-
-    res.status(200).json({
-      status : 'success',
-      data : {
-        user
-      }
-    });
-};
-
-// Similar to all routes in Tours 
+  const user = users.find(el => el.id === id)
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user
+    }
+  })
+}
 
 exports.createUser = (req, res) => {
-    res.status(500).json({status: 'error', message: 'This route is not yet defined!'})
+  const newId = users[users.length - 1].id + 1
+  const newUser = Object.assign({ id: newId }, req.body)
+
+  users.push(newUser)
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/users.json`,
+    JSON.stringify(users),
+    res.status(201).json({
+      success: 'success',
+      data: {
+        user: newUser
+      }
+    })
+  )
 }
+
 exports.updateUser = (req, res) => {
-    res.status(500).json({status: 'error', message: 'This route is not yet defined!'})
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet defined!' })
 }
 exports.deleteUser = (req, res) => {
-    res.status(500).json({status: 'error', message: 'This route is not yet defined!'})
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet defined!' })
 }
