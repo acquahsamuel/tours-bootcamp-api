77563bfd28851a8291c71bbe314ba6f1380a172d
acquahsamuel <micropplejame01@gmail.com>
2020-07-07 14:02:23 +0000

    APIFeature Class

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,70 @@
 const Tour = require('./../models/tourModel')
 
 // Create Tour (single Tour)
+class APIFeatures {
+  constructor(query, queryString) {
+    // query
+    this.query = query
+
+    // req.query
+    this.queryString = queryString
+  }
+
+  filter() {
+    const queryObj = {
+      ...this.queryString
+    }
+    const excludedFields = ['page', 'sort', 'limit', 'fields']
+    excludedFields.forEach(el => delete queryObj[el])
+
+    // 1B Advanced Filtering
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+    console.log(JSON.parse(queryStr))
+
+    this.query = this.query.find(JSON.parse(queryStr))
+
+    return this
+  }
+
+  // 2 Sorting
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ')
+      console.log(sortBy)
+      this.query = this.query.sort(sortBy)
+    } else {
+      // (-) including
+      this.query = this.query.sort('-createdAt')
+    }
+
+    return this
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ')
+      this.query = this.query.select(fields)
+    } else {
+      // (-V) Excluding
+      this.query = this.query.select('-__v')
+    }
+    return this
+  }
+
+  paginate() {
+    // 4) Pagination
+    // Convert string to a number trick
+    const page = this.queryString.page * 1 || 1
+    const limit = this.queryString.limit * 1 || 100
+    const skip = (page - 1) * limit
+
+    this.query = this.query.skip(skip).limit(limit)
+
+    return this
+  }
+}
+
 exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body)
@@ -11,17 +75,27 @@ exports.createTour = async (req, res) => {
       }
     })
   } catch (err) {
-    res.status(400).json({
-      status: 'failed',
-      message: 'Invalid data sent'
-    })
+    res.status(400).json({ status: 'failed', message: 'Invalid data sent' })
   }
+}
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5'
+  req.query.sort = '-ratingsAverage,price'
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
+  next()
 }
 
 // Get all Tours
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find()
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
+    const tours = await features.query
+
     res.status(200).json({
       status: 'succcess',
       results: tours.length,
@@ -30,14 +104,13 @@ exports.getAllTours = async (req, res) => {
       }
     })
   } catch (err) {
-    res.status(400).json({
-      status: 'failed',
-      message: `Oops something happend ${err}`
-    })
+    res
+      .status(400)
+      .json({ status: 'failed', message: `Oops something happend ${err}` })
   }
 }
 
-//Get tour (By ID)
+// Get tour (By ID)
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id)
@@ -49,10 +122,9 @@ exports.getTour = async (req, res) => {
       }
     })
   } catch (err) {
-    res.status(400).json({
-      status: 'failed',
-      message: `Oops something happend ${err}`
-    })
+    res
+      .status(400)
+      .json({ status: 'failed', message: `Oops something happend ${err}` })
   }
 }
 
@@ -69,10 +141,9 @@ exports.updateTour = async (req, res) => {
       }
     })
   } catch (err) {
-    res.status(400).json({
-      status: 'failed',
-      message: `Oops something happend ${err}`
-    })
+    res
+      .status(400)
+      .json({ status: 'failed', message: `Oops something happend ${err}` })
   }
 }
 
@@ -80,14 +151,10 @@ exports.updateTour = async (req, res) => {
 exports.deleteTour = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id)
-    res.status(204).json({
-      status: 'success',
-      data: null
-    })
+    res.status(204).json({ status: 'success', data: null })
   } catch (err) {
-    res.status(400).json({
-      status: 'failed',
-      message: `Oop something happend ${err}`
-    })
+    res
+      .status(400)
+      .json({ status: 'failed', message: `Oop something happend ${err}` })
   }
 }
--- models/tourModel.js
+++ models/tourModel.js
@@ -54,7 +54,8 @@ const tourSchema = mongoose.Schema({
   images: [String],
   createdAt: {
     type: Date,
-    default: Date.now()
+    default: Date.now(),
+    select: false
   },
   startDates: [Date]
 })
