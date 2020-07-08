f9f95661437bb536e95e6accc034a1e4e672aa10
acquahsamuel <micropplejame01@gmail.com>
2020-07-08 04:46:34 +0000

    routes update

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,6 +8,9 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours)
 
+router.route('/tours-stats').get(tourController.getTourStats)
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+
 router
   .route('/')
   .get(tourController.getAllTours)
