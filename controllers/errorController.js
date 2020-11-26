e0dcf069e8126c0a8e9c8fc39377425572789084
acquahsamuel <acquahsamuel1996@gmail.com>
2020-11-26 14:14:17 +0000

    errorHandling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,24 @@
-const sendErroDev = (err, res) => {
+const AppError = require('../utils/appError')
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`
+  return new AppError(message, 400)
+}
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
+  const message = `Duplicate field value : ${value}. Please use another value!`
+  return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message)
+
+  const message = `Invalid input data ${errors.join('.')}`
+  return new AppError(message, 400)
+}
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -9,32 +29,36 @@ const sendErroDev = (err, res) => {
 
 const sendErrorProd = (err, res) => {
   /**Operational , trusted error : send message to client */
-  if(err.isOperational){
-
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message
     })
 
-
     /** Programming or other unknow error : don't expose error details  */
-  }else{
-
+  } else {
     /**Log Error */
-    console.error('ERROR', err);
+    console.error('ERROR', err)
 
-    res.status(500).json({status : 'error', message : 'Something went very wrong'})
+    res
+      .status(500)
+      .json({ status: 'error', message: 'Something went very wrong' })
   }
-
 }
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || `error`
 
-  if ((process, env.NODE_ENV === 'production')) {
-    sendErroDev(err, res)
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res)
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res)
+    let error = { ...err }
+
+    if (err.name === 'CastError') error = handleCastErrorDB(error)
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error)
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error)
+
+    sendErrorProd(error, res)
   }
 }
