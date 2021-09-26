e538654676dcf4aeb00e357dfa4f75714b4cb69b
acquahsamuel <acquahsamuel1996@gmail.com>
2021-09-26 15:59:45 +0000

    [WIP] +> controllers: userController,viewController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,67 @@
+const multer = require('multer');
+const sharp = require('sharp');
+const factory = require("./handlerFactory");
 const User = require("./../models/userModel");
 const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/catchAsync");
-const factory = require("./handlerFactory");
+
+
+/**
+ * @desc            Image upload config
+ * @route           GET /middleware/
+ * @access          Private
+ */
+
+
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     //user-767633abc76dba-33323.jpeg 
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   }
+// });
+
+const multerStorage = multer.memoryStorage();
+
+//check if the file type is image
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+exports.uploadUserPhoto = upload.single('photo');
+
+
+
+/**
+ * @desc            Resizing user photo
+ * @route           GET  middleware
+ * @access          Public
+ */
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  //resize = width && height
+  sharp(req.file.buffer).resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/user/${req.file.filename}`);
+    next();
+};
+
 
 /**
  * @desc            Get all tours
@@ -24,10 +84,10 @@ const filterObj = (obj, ...allowedFields) => {
  * @access          Public
  */
 
- exports.getMe = (req, res , next) =>{
-   req.params.id = req.user.id;
-   next();
- }
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+}
 
 
 /**
@@ -46,6 +106,8 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
 
   const filteredBody = filterObj(req.body, "name", "email");
+  if (req.file) filteredBody.photo = req.file.filename;
+
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,14 +1,14 @@
 const Tour = require("../models/tourModel");
-const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 
 exports.getOverview = catchAsync(async (req, res) => {
   const tours = await Tour.find();
-
   res.status(200).render("overview", {
     tours
   });
 });
+
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
@@ -31,6 +31,7 @@ exports.getloginForm = catchAsync(async (req, res, next) => {
     title: "Login into your account"
   });
 });
+
 
 exports.getAccount = catchAsync(async (req, res) => {
   res.status(200).render("account", {
